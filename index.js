add876152279343bf2909c7292089f78af94ecce
Daniel Casita <legendadtwo@gmail.com>
2024-09-11 18:30:07 -0500

    Se agregan ovejas al hacer clic en +

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const count = document.getElementById("count");
 const btnAdd = document.getElementById("add");
 const btnLess = document.getElementById("less");
 const btnReset = document.getElementById("reset");
+const sheepContainer = document.getElementById("sheep-img")
 
 let countValue = 0;
 
 btnAdd.onclick = function(){
     countValue++;
     count.innerHTML = countValue
+
+    const sheepImg = document.createElement('img');
+    sheepImg.src = 'images/sheep.png';
+    sheepImg.alt = 'Oveja';
+    sheepImg.className = 'sheep';
+    sheepContainer.appendChild(sheepImg);
 }
 
 btnLess.onclick = function(){
